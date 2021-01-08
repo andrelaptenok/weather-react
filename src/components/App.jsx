65fda937a95adfecd715cc6d7fb8256821d9e886
andrelaptenok <andrelaptenok@gmail.com>
2021-01-08 19:52:41 +0300

    add app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,86 @@
 import React, { useState } from 'react';
 
-export const App = () =>{
-    const [query, setQuery] = useState('');
-    const [weather, setWeather] = useState({});
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
 
-    const apiKey = `${process.env.REACT_APP_WEATHER_API}`
-  
-    const search = evt => {
-      if (evt.key === "Enter") {
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&APPID=${apiKey}`)
-          .then(res => res.json())
-          .then(result => {
-            setWeather(result);
-            setQuery('');
-            console.log(result);
-          });
-      }
+  const apiKey = `${process.env.REACT_APP_WEATHER_API}`;
+
+  const search = (evt) => {
+    if (evt.key === 'Enter') {
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&APPID=${apiKey}`,
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery('');
+        });
     }
-  
-    const dateBuilder = (d) => {
-      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  
-      let day = days[d.getDay()];
-      let date = d.getDate();
-      let month = months[d.getMonth()];
-      let year = d.getFullYear();
-  
-      return `${day}, ${date} ${month}, ${year}`
-    }
-  
-    return (
-      <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app warm'}>
-        <main>
-          <div className="search-box">
-            <input 
-              type="text"
-              className="search-bar"
-              placeholder="Search..."
-              onChange={e => setQuery(e.target.value)}
-              value={query}
-              onKeyPress={search}
-            />
-          </div>
-          {(typeof weather.main != "undefined") ? (
+  };
+
+  const dateBuilder = (d) => {
+    let months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day}, ${date} ${month}, ${year}`;
+  };
+
+  return (
+    <div
+      className={
+        typeof weather.main != 'undefined'
+          ? weather.main.temp > 16
+            ? 'app warm'
+            : 'app'
+          : 'app warm'
+      }>
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {typeof weather.main != 'undefined' ? (
           <div>
             <div className="location-box">
-              <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
             <div className="weather-box">
-              <div className="temp">
-                {Math.round(weather.main.temp)}°c
-              </div>
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
               <div className="weather">{weather.weather[0].main}</div>
             </div>
           </div>
-          ) : ('')}
-        </main>
-      </div>
-    );
-}
+        ) : (
+          ''
+        )}
+      </main>
+    </div>
+  );
+};
